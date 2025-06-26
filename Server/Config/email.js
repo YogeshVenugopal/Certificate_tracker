64a1a10c118534ee987a3f1e98ffce95476cfcad
rxnjitth <mranjith2506@gmail.com>
2025-06-26 14:29:26 +0530

    email.js updated :))

--- Server/Config/email.js
+++ Server/Config/email.js
@@ -1,22 +1,22 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
+  import nodemailer from "nodemailer";
+  import dotenv from "dotenv";
+  dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    logger: true,
-    debug: true,
-    requireTLS: false,
-    auth: {
-      user: process.env.SENDER_MAIL,
-      pass: process.env.SMTP_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      logger: true,
+      debug: true,
+      requireTLS: false,
+      auth: {
+        user: process.env.SENDER_MAIL,
+        pass: process.env.SMTP_PASS
+      },
+      tls: {
+          rejectUnauthorized: true
+      }
+    });
 
-export default transporter;
+  export default transporter;
